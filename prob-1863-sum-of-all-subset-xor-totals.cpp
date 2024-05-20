4e6527666c9dc2b43c47ea59b9326683aee40bad
Piyush Katyal <109459034+piyushk77@users.noreply.github.com>
2024-05-20 09:53:49 +0530

    chore: improve prob 1863

--- prob-1863-sum-of-all-subset-xor-totals.cpp
+++ prob-1863-sum-of-all-subset-xor-totals.cpp
@@ -4,28 +4,15 @@ using namespace std;
 class Solution
 {
 public:
+    int recXorSum(vector<int> &nums, int n, int i, int sum)
+    {
+        if (i >= n)
+            return sum;
+        return recXorSum(nums, n, i + 1, sum) + recXorSum(nums, n, i + 1, sum ^ nums[i]);
+    }
     int subsetXORSum(vector<int> &nums)
     {
-        vector<vector<int>> powerSet{{}};
-        for (auto x : nums)
-        {
-            int n{static_cast<int>(powerSet.size())};
-            for (int i{}; i < n; ++i)
-            {
-                vector<int> subSet{powerSet[i]};
-                subSet.push_back(x);
-                powerSet.push_back(subSet);
-            }
-        }
-
-        int sum{};
-        for (auto x : powerSet)
-        {
-            int xorSum{};
-            for (auto y : x)
-                xorSum ^= y;
-            sum += xorSum;
-        }
-        return sum;
+        int n{static_cast<int>(nums.size())};
+        return recXorSum(nums, n, 0, 0);
     }
 };
