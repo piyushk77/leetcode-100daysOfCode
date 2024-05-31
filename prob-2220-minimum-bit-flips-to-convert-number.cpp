3a61e43f3342cd3235b1ecea8bd695f9eb989f32
Piyush Katyal <109459034+piyushk77@users.noreply.github.com>
2024-05-31 12:59:12 +0530

    feat: add day 152 solution

---
+++ prob-2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int size {sizeof(int)*8}, count {};
+        int mask {1};
+        for(int i {}; i < size; ++i){
+            if((static_cast<bool>(mask & goal)) ^ (static_cast<bool>(mask & start)))
+                ++count;
+            mask <<= 1;
+        }
+        return count;
+    }
+};
