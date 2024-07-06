2566b0c658fb656f0f23ac7567e4086d26a55ed8
Piyush Katyal <109459034+piyushk77@users.noreply.github.com>
2024-07-06 15:53:59 +0530

    feat/chore: add day 188 solution

--- prob-2582-pass-the-pillow.cpp
+++ prob-2582-pass-the-pillow.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        return ((static_cast<int>(!((time / (n - 1)) % 2))) *
-                    ((time % (n - 1)) + 1) +
-                (static_cast<int>((time / (n - 1)) % 2)) *
-                    (n - (time % (n - 1))));
+        if ((time / (n - 1)) % 2)
+            return n - (time % (n - 1));
+        return 1 + (time % (n - 1));
     }
 };
