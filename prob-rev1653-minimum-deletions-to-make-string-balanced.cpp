21268f2e3bae7aa3e543dcf152ddcf3906fcb533
Piyush Katyal <109459034+piyushk77@users.noreply.github.com>
2024-07-30 11:53:55 +0530

    feat: add day 212 solution

---
+++ prob-rev1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <limits.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumDeletions(string s)
+    {
+        int aCount = 0;
+        for (auto &ch : s)
+            aCount += (ch == 'a');
+        int minDeletions = INT_MAX;
+        int bCount = 0;
+        for (auto &ch : s)
+        {
+            aCount -= (ch == 'a');
+            minDeletions = min(minDeletions, aCount + bCount);
+            bCount += (ch == 'b');
+        }
+        return minDeletions;
+    }
+};
